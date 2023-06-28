7e71acf61d3672c57d279b2ef1a17e25b773a721
Your Name~ <you@example.com>
2023-06-28 17:57:47 +0300

    save

--- js/reviews.js
+++ js/reviews.js
@@ -1,37 +1,37 @@
-var prev = document.getElementById('prev');
-var next = document.getElementById('next');
-var slider = document.getElementById('slider');
-var total = 0, step = 100;
+let position = 0;
+const slidesToShow = 2;
+const slidesToScroll = 1;
+const container = document.querySelector('.reviews_slider-container');
+const track = document.querySelector('.reviews_slider-track');
+const btnPrev = document.querySelector('.reviews_btn-prev');
+const btnNext = document.querySelector('.reviews_btn-next');
+const items = document.querySelectorAll('.reviews_slider-item')
+const itemsCount = items.length;
+const itemWidth = container.clientWidth / slidesToShow;
+const movePosition = slidesToScroll * itemWidth;
 
-prev.addEventListener('click', slide);
-next.addEventListener('click', slide);
+items.forEach((item) => {
+    item.style.minWidth = `${itemWidth}px`
+})
 
-function slide()
-{
-  if(this.getAttribute('id') == 'prev')
-  {
-    if(total == 0)
-    {
-      total = -400;
-      slider.style.left = total + '%';
-    }
-    else
-    {
-      total += step;
-      slider.style.left = total + '%';
-    }
-  }
-  else
-  {
-    if(total == -400)
-    {
-      total = 0;
-      slider.style.left = total;
-    }
-    else
-    {
-      total -= step;
-      slider.style.left = total + '%';
-    }
-  }
+btnNext.addEventListener('click',() => {
+    const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
+    position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+    setPosition()
+    checkBtns()
+})
+
+btnPrev.addEventListener('click',() => {
+    const itemsLeft = Math.abs(position) / itemWidth
+    position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
+    setPosition()
+    checkBtns()
+});
+const setPosition = () => {
+    track.style.transform = `translateX(${position}px)`
 }
+const checkBtns = () => {
+    btnPrev.disabled = position === 0
+    btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth
+}
+    checkBtns()
