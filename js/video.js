5344f22d68b8a40f6d91c1bd5275cebdde6f432f
Your Name~ <you@example.com>
2023-06-29 23:24:50 +0300

    save

---
+++ js/video.js
@@ -0,0 +1,10 @@
+
+function openPopupVideo() {
+    var popupContainer = document.getElementById("popupContainer");
+    popupContainer.style.display = "block";
+}
+
+function closePopupVideo() {
+    var popupContainer = document.getElementById("popupContainer");
+    popupContainer.style.display = "none";
+}
